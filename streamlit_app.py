f82062cca388ea35a7a1cdbd78d4e8f4c135246d
AkmalAbbas5129 <157809723+AkmalAbbas5129@users.noreply.github.com>
2024-08-30 17:04:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,231 +1,110 @@
-import os
 import streamlit as st
-import base64
-from dotenv import load_dotenv
-from langchain_openai import AzureChatOpenAI
-from crewai import Agent, Task, Crew, Process
-from langchain_core.tools import Tool
-from langchain_experimental.utilities import PythonREPL
+import time
+from utils import generate_data_info_in_natural_language,generate_optimization
+from supply_chain_scenarios.custom_order_fullfilment import generate_customer_order_fulfillment_predictions
+from supply_chain_scenarios.demand_supply_matching import generate_demand_supply_matching_predictions
+from supply_chain_scenarios.supplier_risk_management import generate_supplier_risk_predictions
+from supply_chain_scenarios.demand_forecasting import generate_demand_forecasting_predictions
+from langgraph_crew import generate_report_for_scenario
 
-# Load environment variables
-load_dotenv()
 
-os.environ["AZURE_OPENAI_API_KEY"] = st.secrets["openai_api_key"]
-os.environ["AZURE_OPENAI_ENDPOINT"] = st.secrets["azure_endpoint"]
-os.environ["AZURE_OPENAI_API_VERSION"] = st.secrets["api_version"]
-os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"] = st.secrets["deployment_name"]
-os.environ["OPENAI_MODEL_NAME"]="gpt-4o"
+# Function to simulate fetching predictions from an ML model
+def get_dummy_predictions(scenario):
+    problem_objective_constraint = ""
+    data_predicted = ""
 
-# Initialize Azure OpenAI LLM
-azure_llm = AzureChatOpenAI(
-    openai_api_version=st.secrets["api_version"],
-    azure_deployment=st.secrets["deployment_name"],
-    model="gpt-4o"
-)
+    with st.spinner('Predictions are being fetched...'):
+        if scenario == "Customer Order Fulfillment":
+            data_predicted = generate_customer_order_fulfillment_predictions()
+            problem_objective_constraint = generate_optimization(scenario, data_predicted)
+        elif scenario == "Demand-Supply Matching":
+            data_predicted = generate_demand_supply_matching_predictions()
+            problem_objective_constraint = generate_optimization(scenario, data_predicted)
+        elif scenario == "Supplier Risk Assessment":
+            data_predicted = generate_supplier_risk_predictions()
+            problem_objective_constraint = generate_optimization(scenario, data_predicted)
+        elif scenario == "Demand Forecasting":
+            data_predicted = generate_demand_forecasting_predictions()
+            problem_objective_constraint = generate_optimization(scenario, data_predicted)
 
-# Initialize Python REPL tool
-python_repl = PythonREPL()
-coding_agent_tool = Tool(
-    name="python_repl",
-    description="A Python shell. Use this to execute python commands. Input should be a valid python command in Python REPL dictionary format. If you want to see the output of a value, you should print it out with `print(...)`.",
-    func=python_repl.run,
-)
+    nl_formatted_data = "No Data"  # generate_data_info_in_natural_language(scenario, data_predicted)
+    return nl_formatted_data, data_predicted, problem_objective_constraint
 
-# Set the layout to wide
+
+# Function to generate a problem statement
+def generate_problem_statement():
+    return "This is the generated problem statement based on the selected scenario."
+
+
+# Set up the layout
 st.set_page_config(layout="wide")
 
-# Custom CSS to adjust the width of the markdown container
-st.markdown(
-    """
-    <style>
-    .reportview-container .main .block-container{
-        max-width: 100%;
-        padding: 0;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Sidebar with Logo and Scenario Dropdown
+with st.sidebar:
+    st.markdown(
+        """
+        <style>
+        .center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100px;
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-# Function to convert image to base64
-def get_image_base64(image_path):
-    with open(image_path, "rb") as image_file:
-        img_str = base64.b64encode(image_file.read()).decode()
-    return img_str
+    # Centered Logo
+    st.markdown('<div class="center"><img src="systemsltd.png" width="150" /></div>', unsafe_allow_html=True)
+    st.header("Supply Chain Scenario")
 
-# Paths to your local icons
-icon1_path = "crewai.jpg"  # Replace with the path to your first icon
-icon2_path = "streamlit.png"  # Replace with the path to your second icon
-sidebar_logo_path = "systemsltd.png"  # Replace with the path to your sidebar logo
+    # Scenario Dropdown
+    scenario = st.selectbox(
+        "Select Scenario:",
+        ["", "Customer Order Fulfillment", "Demand-Supply Matching", "Supplier Risk Assessment","Demand Forecasting"],
+        key="scenario"
+    )
 
-# Convert icons to base64
-icon1_base64 = get_image_base64(icon1_path)
-icon2_base64 = get_image_base64(icon2_path)
-sidebar_logo_base64 = get_image_base64(sidebar_logo_path)
+# Main Content Area
+if scenario:
+    # Check if the selected scenario has changed
+    if "selected_scenario" in st.session_state and st.session_state["selected_scenario"] != scenario:
+        # Clear the report section when scenario changes
+        st.session_state["report"] = ""
 
-# Function to create image HTML tag
-def image_html(base64_str, height="50px"):
-    return f'<img src="data:image/png;base64,{base64_str}" style="height:{height}; margin-left:10px;">'
+    # Check if data for the selected scenario is already in session state
+    if "data" not in st.session_state or "problem_statement" not in st.session_state or st.session_state[
+        "selected_scenario"] != scenario:
+        # Show spinner while fetching predictions
+        with st.spinner('Fetching predictions...'):
+            st.session_state["predictions"], st.session_state["data"], st.session_state[
+                "problem_statement"] = get_dummy_predictions(scenario)
+        st.session_state["selected_scenario"] = scenario
 
-# Title with icons
-title_html = f"""
-<h1 style="display: flex; align-items: center;">
-    OptiGenius  .  .  . {image_html(icon1_base64)} {image_html(icon2_base64)}
-</h1>
-"""
+    # Display the scenario title
+    st.title(f"Scenario: {scenario}")
 
-# Example problems
-examples = {
-    "Fly High Airlines": """
-Fly High Airlines sells business class and tourist class seats for its charter flights. To charter a plane, at least 5 Business class tickets must be sold and at least 9 tourist class tickets must be sold. The plane does not hold more than 30 passengers. Fly-High makes $40 profit for each business class ticket sold and $45 profit for each tourist class ticket sold. For Fly-High Airlines to maximize profits, how many tourists class seats should they sell?
+    # Fetch and display dummy predictions in a text area
+    predictions = st.session_state["predictions"]
+    data = st.session_state["data"]
+    problem_statement = st.session_state["problem_statement"]
 
-Objective:
-How many tourist class seats should they sell to maximize the profits?
+    st.subheader("Data")
+    st.text_area("Data", data, height=75)
+    # st.subheader("ML Model Predictions in Natural Language Format")
+    # st.text_area("Predictions", predictions, height=200)
 
-Constraints:
-- Plane does not hold more than 30 passengers.
-- At least 5 business class tickets must be sold.
-- At least 9 tourist class tickets must be sold.
-""",
-    "ABC E-commerce Company": """
-ABC E-commerce Company needs to determine the optimal number of ads to place on Social Media, Search Engines, and Display Ads to maximize the number of new customers, given budget constraints and platform-specific limits.
+    # Display Problem Statement and Report Text Areas
+    st.subheader("Generated Problem Statement")
+    problem_statement_area = st.text_area("Problem Statement", problem_statement, height=300)
 
-Objective:
-Maximize the total number of new customers acquired from advertising.
+    st.subheader("Generated Report")
+    report = st.session_state.get("report", "This is the generated report based on the scenario and predictions.")
+    report_area = st.text_area("Report", report, height=300)
 
-Constraints:
-- Budget Constraint: The total advertising budget is $50,000.
-- Cost per Ad:
-  - Social Media: $200 per ad.
-  - Search Engines: $300 per ad.
-  - Display Ads: $250 per ad.
-- Expected New Customers per Ad:
-  - Social Media: 50 new customers per ad.
-  - Search Engines: 80 new customers per ad.
-  - Display Ads: 60 new customers per ad.
-- Non-Negativity Constraint: The number of ads placed on each platform must be non-negative.
-"""
-}
-
-# Title and description
-st.markdown(title_html, unsafe_allow_html=True)
-st.write("OptiGenius, a multi-agent system powered by CrewAI and Streamlit. It uses a multi-agent approach and code execution tools incorporated in CrewAI to solve Resource Optimization Problems.")
-
-# Dropdown menu for selecting an example problem
-problem_selection = st.selectbox("Select an example problem:", ["", "Fly High Airlines", "ABC E-commerce Company"])
-
-# Text input field
-if problem_selection:
-    user_input = st.text_area("Enter Problem Statement:", examples[problem_selection], height=300)
-else:
-    user_input = st.text_area("Enter Problem Statement:", height=300)
-
-# Create two columns for the buttons
-col1, col2 = st.columns(2)
-
-### Create an agent with code execution enabled
-coding_agent = Agent(
-    role='Software Engineer',
-    goal='Solve resource optimization problem using Pulp library and execute using code execution.',
-    backstory='You are a helpful AI assistant. Solve tasks using your coding and language skills. You must ensure that the code is properly formatted and wrapped according to Python REPL executor. For Example: ```{"command": "from pulp import LpMaximize, LpProblem, LpVariable, lpSum, value\n"}```.',
-    verbose=True,
-    tools=[coding_agent_tool],
-    allow_code_execution=True,
-    llm=azure_llm
-)
-
-# Task for solving the optimization problem
-cli_task = Task(
-    description=user_input,
-    agent=coding_agent,
-    expected_output='Desired output in python formatted context',
-)
-
-report_agent = Agent(
-    role='Technical Writer',
-    goal='Generate a concise report based on the given solution.',
-    backstory='You are an expert in creating structured and concise reports based on provided data. Your goal is to present the information in a clear and engaging manner.',
-    memory=True,
-    verbose=True,
-    llm=azure_llm
-)
-
-# Task for generating the report
-report_task = Task(
-    description="""Generate a concise report based on the previous given solution with the following headings:
-    1. Problem Statement
-    2. Constraints
-    3. Optimization Answer
-    4. Conclusion
-    """,
-    agent=report_agent,
-    expected_output='''A structured report in markdown format of the solution with the following headings as an Final Answer:
-    1. Problem Statement
-    2. Constraints
-    3. Optimization Answer
-    4. Conclusion'''
-)
-
-# Submit button
-with col1:
-    if st.button("Submit"):
-        # Initialize progress bar
-        progress_bar = st.progress(0)
-        status_text = st.empty()
-
-        # Display a spinner while the solution is being generated
-        with st.spinner("Processing your request... Please wait."):
-            # Update the status for initializing the agent
-            progress_bar.progress(10)
-            status_text.markdown("**Initializing agents...**")
-
-            # Create a crew and add the task
-            analysis_crew = Crew(
-                agents=[coding_agent, report_agent],
-                tasks=[cli_task, report_task],
-                process=Process.sequential,
-            )
-
-            # Update the status for starting the task
-            progress_bar.progress(30)
-            status_text.markdown("**Starting task execution...**")
-
-            # Execute the crew
-            result = analysis_crew.kickoff()
-
-            # Update the status during task execution
-            progress_bar.progress(70)
-            status_text.markdown("**Generating solution...**")
-
-            try:
-                os.remove('trained_agents_data.pkl')
-            except Exception as ex:
-                print("Exception has occurred")
-
-            # Update the status when the task is complete
-            progress_bar.progress(100)
-            status_text.markdown("**Task completed successfully!**")
-
-        # Display the result in a styled container
-        st.markdown("## 🧠 Optimization Solution")
-        st.markdown(
-            """
-            <div style="
-                background-color: #f9f9f9;
-                padding: 20px;
-                border-radius: 10px;
-                box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
-            ">
-                <h3 style="color: #2c3e50;">Result:</h3>
-                <p style="font-size: 16px; color: #34495e;">
-                    {}</p>
-            </div>
-            """.format(result),
-            unsafe_allow_html=True
-        )
-
-# Clear button
-with col2:
-    if st.button("Clear"):
-        # Clear the input field (refresh the page)
-        st.rerun()
+    # Button to trigger optimization (generating the report)
+    if st.button("Optimize"):
+        statement_for_scenario = problem_statement_area
+        report = generate_report_for_scenario(scenario, statement_for_scenario)
+        st.session_state["report"] = report  # Store the generated report in session state
+        st.rerun()  # Rerun the script to update the report text area with the new report
