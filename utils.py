f631db4944c0aa5d4e95963c33b540860a9a0242
AkmalAbbas5129 <157809723+AkmalAbbas5129@users.noreply.github.com>
2024-09-05 16:30:26 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ os.environ["AZURE_OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 os.environ["AZURE_OPENAI_ENDPOINT"] = st.secrets["azure_endpoint"]
 os.environ["AZURE_OPENAI_API_VERSION"] = st.secrets["api_version"]
 os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"] = st.secrets["deployment_name"]
-os.environ["OPENAI_MODEL_NAME"]="gpt-4o"
+os.environ["OPENAI_MODEL_NAME"] = "gpt-4o"
 
 # Initialize Azure OpenAI LLM
 azure_llm = AzureChatOpenAI(
@@ -15,6 +15,7 @@ azure_llm = AzureChatOpenAI(
     azure_deployment=st.secrets["deployment_name"],
     model="gpt-4o"
 )
+
 
 def generate_optimization(scenario, data):
     sys_prompt = """
@@ -66,7 +67,62 @@ def generate_optimization(scenario, data):
     return ans.content
 
 
-def generate_data_info_in_natural_language(scenario,data):
+def explain_solution(problem_statement, result):
+    system = """
+    I will give you optimization problem which is solved by using linear programming python tool PuLP.
+    Give answer to the objective and problem statement by looking at optimization results. Don't output anything extra.
+    """
+
+    # system = """
+    # You are an expert in writing reports in such a way that any one who reads it can easily understand it.
+    # Please use proper formatting and easy to understand vocabulary to write a report. Always use plain english and
+    # not use any latex or other expressions to show calculations.
+    #
+    # Your job is to understand the result of the code execution according to the Problem Statements,Objective and Constraint,
+    # and then give an answer to the user in the form of report.
+    #
+    # 1. Always end with conclusion with respect to objective by explaining problem statement and constraints.
+    # 2. Give suggestions as an expert.
+    # """
+    #
+    # human_message = """
+    # Please write a report according to following.
+    #
+    # Problem Statements,Objective and Constraint:
+    # {problem}
+    #
+    # Result of Code Execution according to problem:
+    # {result}
+    #
+    # Report:
+    # [Write report here to show beautifully compatible in markdown format]
+    # """
+
+    human_message = """
+    Optimization Problem:
+    {problem}
+
+    Optimal Results:
+    {result}
+    """
+
+    prompt = ChatPromptTemplate.from_messages(
+        [("system", system), ("human", human_message)]
+    )
+
+    chain = prompt | azure_llm
+
+    data_in_nl = chain.invoke(
+        {
+            "problem": problem_statement,
+            "result": result
+        }
+    )
+
+    return data_in_nl.content
+
+
+def generate_data_info_in_natural_language(scenario, data):
     system = """
     I will give you predictions from Machine Learning Model for a Scenario. Write Predicted data in natural language. 
     """
